5554078cb6295fe963916cd7ff45e7dc5be0a980
BigTuna94 <zach.richard94@gmail.com>
2016-02-16 20:36:12 -0500

    used for testing changes to the MAX232 circuit

---
+++ rpi/SerialMOD/serial_test.py
@@ -0,0 +1,18 @@
+import serial
+import array
+PORT = "/dev/ttyUSB0"
+BAUD = 9600#19200
+#TIMEOUT = 0.5
+
+data = array.array('B', [0x11]).tostring()
+
+s = serial.Serial( PORT, baudrate=BAUD)
+
+inp = ""
+go = 1
+while go:
+	s.write(data)
+	#inp = raw_input("")
+	#if(inp == "stop"):
+	#	go = 0
+s.close()
