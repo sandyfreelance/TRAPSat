9d2bce1eda9abc709819aa60bc5e7ec4033479e2
BigTuna94 <zach.richard94@gmail.com>
2016-03-10 22:37:36 -0500

    Added VC0706 cFS App shell

---
+++ TRAPSat_cFS/cfs/apps/vc0706/fsw/src/vc0706_events.h
@@ -0,0 +1,28 @@
+/************************************************************************
+** File:
+**    vc0706_events.h 
+**
+** Purpose: 
+**  Define VC0706 App Events IDs
+**
+** Notes:
+**
+**
+*************************************************************************/
+#ifndef _vc0706_events_h_
+#define _vc0706_events_h_
+
+
+#define SAMPLE_RESERVED_EID              0
+#define SAMPLE_STARTUP_INF_EID           1 
+#define SAMPLE_COMMAND_ERR_EID           2
+#define SAMPLE_COMMANDNOP_INF_EID        3 
+#define SAMPLE_COMMANDRST_INF_EID        4
+#define SAMPLE_INVALID_MSGID_ERR_EID     5 
+#define SAMPLE_LEN_ERR_EID               6 
+
+#endif /* _vc0706_events_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
