3d5c4ab51215b602d9c64122dc9aa8334d877fac
BigTuna94 <zach.richard94@gmail.com>
2016-03-11 14:43:53 -0500

    Replacing Sample Code

--- TRAPSat_cFS/cfs/apps/vc0706/fsw/src/vc0706_events.h
+++ TRAPSat_cFS/cfs/apps/vc0706/fsw/src/vc0706_events.h
@@ -13,13 +13,13 @@
 #define _vc0706_events_h_
 
 
-#define SAMPLE_RESERVED_EID              0
-#define SAMPLE_STARTUP_INF_EID           1 
-#define SAMPLE_COMMAND_ERR_EID           2
-#define SAMPLE_COMMANDNOP_INF_EID        3 
-#define SAMPLE_COMMANDRST_INF_EID        4
-#define SAMPLE_INVALID_MSGID_ERR_EID     5 
-#define SAMPLE_LEN_ERR_EID               6 
+#define VC0706_RESERVED_EID              0
+#define VC0706_STARTUP_INF_EID           1 
+#define VC0706_COMMAND_ERR_EID           2
+#define VC0706_COMMANDNOP_INF_EID        3 
+#define VC0706_COMMANDRST_INF_EID        4
+#define VC0706_INVALID_MSGID_ERR_EID     5 
+#define VC0706_LEN_ERR_EID               6 
 
 #endif /* _vc0706_events_h_ */
 
