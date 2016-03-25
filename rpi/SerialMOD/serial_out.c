d494ca099418b638417cd76b04a2b5fba826c743
BigTuna94 <zach.richard94@gmail.com>
2016-03-25 13:20:46 -0400

    Added serial_write_file() function, untested but simple.

--- rpi/SerialMOD/serial_out.c
+++ rpi/SerialMOD/serial_out.c
@@ -20,3 +20,28 @@ void serial_write_byte(serial_out_t *serial, unsigned char byte) // writes byte 
 	serialPutchar(serial->fd, byte);
 	serial->data = byte;
 }
+
+int serial_write_file(serial_out_t *serial, char* file_path)
+{
+	FILE *file_to_write;
+	unsigned char data_buff;
+	file_to_write = fopen(file_path, "r");
+
+	if(file_to_write = NULL)
+	{
+		printf("Error Opening file: %s\n", file_path);
+		return -1;
+	}
+	while(1) // maybe add timeout?
+	{
+		data_buff = (unsigned char) fgetc(file_to_write); // returns int, but is really an unsigned char
+		if(feof(file_to_write))
+		{
+			break; // end of file
+		}
+		serial_write_byte(serial, data_buff); // output to serial
+	}
+	fclose(file_to_write);
+	return 0;
+}
+
